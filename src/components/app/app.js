4d9b6cad89912255e39f41052806c0aad2eda133
gauravprwl14 <gauravprwl14@gmail.com>
2018-05-21 16:15:18 +0530

    initial cra setup for xmpp chat app

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+//styles
+import "./app.scss";
+
+class App extends Component {
+  render() {
+    return <div className="app">Xmpp Chat App</div>;
+  }
+}
+
+export default App;

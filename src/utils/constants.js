e0a183e5a013fc8f876595b61f60a38af1d7a3ec
gauravprwl14 <gauravprwl14@gmail.com>
2018-07-16 00:16:23 +0530

    dont know anything about this commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,46 @@
+// const constantsObj = {
+//   serverUrl: "demo-noturn.t-e.biz",
+//   boshServerUrl: "https://demo-noturn.t-e.biz/http-bind",
+//   conferenceServerUrl: "conference.demo-noturn.t-e.biz"
+// };
+// const constantsObjForCommonClass = {
+//   hosts: {
+//     domain: "jitsi-videobridge.demo-noturn.t-e.biz",
+//     muc: "conference.demo-noturn.t-e.biz",
+//     bridge: "jitsi-videobridge.demo-noturn.t-e.biz"
+//   },
+//   bosh: "https://demo-noturn.t-e.biz/http-bind"
+//   // hosts: {
+//   //   domain: "jitsi.geekydev.com",
+//   //   muc: "conference.jitsi.geekydev.com",
+//   //   bridge: "jitsi-videobridge.jitsi.geekydev.com"
+//   // },
+//   // bosh: "wss://jitsi.geekydev.com:7443/ws/"
+// };
+
+// const constantsObj = {
+//   serverUrl: "teleportation-eu1.t-e.biz",
+//   boshServerUrl: "https://teleportation-eu1.t-e.biz:7443/http-bind/",
+//   conferenceServerUrl: "conference.teleportation-eu1.t-e.biz"
+// };
+
+// const constantsObjForCommonClass = {
+//   hosts: {
+//     domain: "jitsi-videobridge.teleportation-eu1.t-e.biz",
+//     muc: "conference.teleportation-eu1.t-e.biz",
+//     bridge: "jitsi-videobridge.teleportation-eu1.t-e.biz"
+//   },
+//   bosh: "https://teleportation-eu1.t-e.biz:7443/http-bind/"
+//   // hosts: {
+//   //   domain: "jitsi.geekydev.com",
+//   //   muc: "conference.jitsi.geekydev.com",
+//   //   bridge: "jitsi-videobridge.jitsi.geekydev.com"
+//   // },
+//   // bosh: "wss://jitsi.geekydev.com:7443/ws/"
+// };
+
+console.log('%c  ', 'background: lime; color: black');
+
 const constantsObj = {
   serverUrl: "jitsi.geekydev.com",
   boshServerUrl: "wss://jitsi.geekydev.com:7443/ws/",
@@ -5,12 +48,19 @@ const constantsObj = {
 };
 const constantsObjForCommonClass = {
   hosts: {
-    domain: "jitsi.geekydev.com",
-    muc: "conference.jitsi.geekydev.com",
-    bridge: "jitsi-videobridge.jitsi.geekydev.com"
+    domain: "jitsi-videobridge.teleportation-eu1.t-e.biz",
+    muc: "conference.teleportation-eu1.t-e.biz",
+    bridge: "jitsi-videobridge.teleportation-eu1.t-e.biz"
   },
-  bosh: "wss://jitsi.geekydev.com:7443/ws/"
+  bosh: "https://teleportation-eu1.t-e.biz:7443/http-bind/"
+  // hosts: {
+  //   domain: "jitsi.geekydev.com",
+  //   muc: "conference.jitsi.geekydev.com",
+  //   bridge: "jitsi-videobridge.jitsi.geekydev.com"
+  // },
+  // bosh: "wss://jitsi.geekydev.com:7443/ws/"
 };
+
 // const constantsObj = {
 //   serverUrl: "meet.jit.si",
 //   boshServerUrl: "https://meet.jit.si/http-bind",
